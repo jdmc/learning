d7f5e06d22ba4cd8845ccd94a0e7d87c38edda1f
jdmc <josedmc@hotmail.com>
2024-02-19 13:44:58 +0000

    Feb 19, 2024, 2:44 PM

--- Edube/Module2/ejercicio2.py
+++ Edube/Module2/ejercicio2.py
@@ -1,4 +1,7 @@
-""" La tarea es preparar un código simple para evaluar o encontrar el tiempo final de un periodo de tiempo dado, expresándolo en horas y minutos. Las horas van de 0 a 23 y los minutos de 0 a 59. El resultado debe ser mostrado en la consola.
+""" 
+
+La tarea es preparar un código simple para evaluar o encontrar el tiempo final de un periodo de tiempo dado, expresándolo en horas y minutos. 
+Las horas van de 0 a 23 y los minutos de 0 a 59. El resultado debe ser mostrado en la consola.
 
 Por ejemplo, si el evento comienza a las 12:17 y dura 59 minutos, terminará a las 13:16.
 
@@ -10,3 +13,9 @@ Prueba el código cuidadosamente. Pista: utilizar el operador % puede ser clave 
 hour = int(input("Hora de inicio (horas): "))
 mins = int(input("Minuto de inicio (minutos): "))
 dura = int(input("Duración del evento (minutos): "))
+
+minutos = mins + dura
+
+print(minutos)
+
+print("Tiempo final es:", final)
