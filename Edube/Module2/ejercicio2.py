ed3ed0d968c3901b1016e59ca43d045047a033c1
jdmc <josedmc@hotmail.com>
2024-02-19 13:45:46 +0000

    Feb 19, 2024, 2:45 PM

--- Edube/Module2/ejercicio2.py
+++ Edube/Module2/ejercicio2.py
@@ -1,7 +1,7 @@
 """ 
 
 La tarea es preparar un código simple para evaluar o encontrar el tiempo final de un periodo de tiempo dado, expresándolo en horas y minutos. 
-Las horas van de 0 a 23 y los minutos de 0 a 59. El resultado debe ser mostrado en la consola.
+12Las horas van de 0 a 23 y los minutos de 0 a 59. El resultado debe ser mostrado en la consola.
 
 Por ejemplo, si el evento comienza a las 12:17 y dura 59 minutos, terminará a las 13:16.
 
