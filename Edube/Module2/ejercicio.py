7b962a1240067dbb237ea276537c27bae771324d
jdmc <josedmc@hotmail.com>
2024-02-19 13:33:31 +0000

    Feb 19, 2024, 2:33 PM

--- Edube/Module2/ejercicio.py
+++ Edube/Module2/ejercicio.py
@@ -1,5 +1,5 @@
 x = float(input("Ingresa el valor para x: "))
 
-y= (1/((1/((1/((1/x)+x))+x)))+x)# Escribe tu código aquí.
+y= (1/((1/((1/((1/x)+x))+x))+x))# Escribe tu código aquí.
 
 print("y =", y)
