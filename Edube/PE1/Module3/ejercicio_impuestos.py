6e0d1a9fba3d61ddaf75dd1368dd5bf7bd255bbe
jdmc <josedmc@hotmail.com>
2024-02-22 09:21:10 +0000

    Feb 22, 2024, 10:21 AM

---
+++ Edube/PE1/Module3/ejercicio_impuestos.py
@@ -0,0 +1,36 @@
+""" Érase una vez una tierra de leche y miel, habitada por gente feliz y próspera. La gente pagaba impuestos, por supuesto, su felicidad tenía límites. El impuesto más importante, denominado Impuesto Personal de Ingresos (IPI, para abreviar) tenía que pagarse una vez al año y se evaluó utilizando la siguiente regla:
+
+Si el ingreso del ciudadano no era superior a 85,528 pesos, el impuesto era igual al 18% del ingreso menos 556 pesos y 2 centavos (esta fue la llamada exención fiscal ).
+Si el ingreso era superior a esta cantidad, el impuesto era igual a 14,839 pesos y 2 centavos, más el 32% del excedente sobre 85,528 pesos.
+Tu tarea es escribir una calculadora de impuestos.
+
+Debe aceptar un valor de punto flotante: el ingreso.
+A continuación, debe imprimir el impuesto calculado, redondeado a pesos totales. Hay una función llamada round() que hará el redondeo por ti, la encontrarás en el código de esqueleto del editor. """
+
+# income =< 85.528 => low tax else high tax
+
+
+# low_tax = 18% - (556 - 0.02)
+# high_tax = 32% + (14,839 + 0.02)
+
+# total = income + (low/high_)tax
+
+# tax = total - income 
+
+income = float(input("Introduce el ingreso anual:"))
+
+#
+# Escribe tu código aquí.
+#
+
+if income <= 85528:
+    total = round(income * 0.18 - ( 556 - 0.02)) # total 
+    if tax <= 0:
+        tax = 0
+else:
+    total = round(income * 0.32 + (14,839 + 0.02)) # total 
+
+tax = total - income
+
+tax = round(tax, 0)
+print("El impuesto es:", tax, "pesos")
