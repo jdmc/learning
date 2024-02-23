23cc5cfede882f39ac4558b7c6d488ac87ce9230
jdmc <josedmc@hotmail.com>
2024-02-23 16:42:10 +0000

    Feb 23, 2024, 5:42 PM

---
+++ Edube/PE1/Module3/ejercicio_mississipi.py
@@ -0,0 +1,14 @@
+import time
+
+    # Escribe un bucle for que cuente hasta cinco.
+    # Cuerpo del bucle: imprime el número de iteración del bucle y la palabra "Mississippi".
+    # Cuerpo del bucle - usar: time.sleep (1)
+
+# Escribe una función de impresión con el mensaje final.
+
+for i in range(5):
+    
+    print( i , "Mississippi")
+    
+else:
+    print("¡Listos o no, ahí voy!")
