5e0d266af9966e36186e54572b3b25a2bd141fae
jdmc <josedmc@hotmail.com>
2024-02-26 11:45:30 +0000

    Feb 26, 2024, 12:45 PM

---
+++ sesion11/ejercicio_basico1.py
@@ -0,0 +1,13 @@
+"""
+Crea una clase llamada Persona que tenga los siguientes atributos: 
+
+• nombre: un atributo para almacenar el nombre de la persona. 
+• edad: un atributo para almacenar la edad de la persona. 
+
+1.Agrega un método saludar a la clase Persona que imprima un saludo personalizado con el nombre de la persona. 
+2.Crea dos instancias de la clase Persona e inicialízalas con diferentes nombres y edades. 
+3.Llama al método saludar para cada instancia de Persona y verifica que se imprima correctamente el saludo.
+
+"""
+
+persona
