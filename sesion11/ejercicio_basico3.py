dc929ba16ab744482fd2f5893752249f11b95cd6
jdmc <josedmc@hotmail.com>
2024-02-26 13:26:05 +0000

    Feb 26, 2024, 2:26 PM

---
+++ sesion11/ejercicio_basico3.py
@@ -0,0 +1,22 @@
+""" 
+
+Define una clase llamada Libro con los siguientes atributos privados: 
+    
+    • __titulo: un atributo para almacenar el título del libro. 
+    • __autor: un atributo para almacenar el autor del libro. 
+    • __anio_publicacion: un atributo para almacenar el año de publicación del libro. 
+    
+    1.Utiliza propiedades (getters y setters) para acceder y modificar estos atributos privados. 
+    Asegúrate de seguir las convenciones de notación de Python. 
+    2.Implementa un método detalles_libro que imprima los detalles del libro, incluyendo título, autor y año de publicación. 
+    3.Crea una instancia de la clase Libro e inicialízala con valores para el título, autor y año de publicación. 
+    4.Utiliza las propiedades y el método detalles_libro para acceder y mostrar la información del libro.
+    
+     """
+     
+class Libro:
+    def __init__(self, titulo, autor, anio) -> None:
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__anio = anio
+        
