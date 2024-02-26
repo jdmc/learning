88a7b2aeffd721b71a40b338959138e689265b88
jdmc <josedmc@hotmail.com>
2024-02-26 12:48:21 +0000

    Feb 26, 2024, 1:48 PM

---
+++ sesion11/ejercicio_basico2.py
@@ -0,0 +1,24 @@
+""" 
+Define una clase llamada CuentaBancaria con los siguientes atributos privados:
+    • saldo: un atributo para almacenar el saldo de la cuenta (debe comenzar con un valor predeterminado). 
+    • titular: un atributo para almacenar el nombre del titular de la cuenta. 
+    
+    1.Agrega métodos públicos para: 
+        
+        • Obtener el saldo actual (obtener_saldo). 
+        • Realizar un depósito (realizar_deposito) que aumenta el saldo. 
+        • Realizar un retiro (realizar_retiro) que disminuye el saldo. 
+        
+    2.Implementa la encapsulación adecuada para que los atributos saldo y titular solo puedan ser modificados internamente. 
+    3.Crea una instancia de la clase CuentaBancaria e inicialízala con un saldo inicial y el nombre del titular. 
+    4.Realiza algunas operaciones de depósito y retiro utilizando los métodos definidos. 
+    
+    cuenta_bancaria modulo    
+    
+    """
+    
+class CuentaBancaria:
+    def __init__(self, saldo, titular):
+        self__saldo= saldo
+        self__titular = titular
+        pass
