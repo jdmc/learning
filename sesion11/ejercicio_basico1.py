64b8af40270015070d33509ac39f6198784c7aa3
jdmc <josedmc@hotmail.com>
2024-02-26 11:49:59 +0000

    Feb 26, 2024, 12:49 PM

--- sesion11/ejercicio_basico1.py
+++ sesion11/ejercicio_basico1.py
@@ -10,4 +10,25 @@ Crea una clase llamada Persona que tenga los siguientes atributos:
 
 """
 
-persona
+class Persona:
+    def __init__(self, nombre, edad):
+        self.__nombre = nombre
+        self.__edad = edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.__nombre} y tengo {self.__edad} años.")
+
+# Crear dos instancias de Persona
+persona1 = Persona("Juan", 30)
+persona2 = Persona("María", 25)
+
+# Llamar al método saludar para cada instancia
+persona1.saludar()  # Imprime: Hola, soy Juan y tengo 30 años.
+persona2.saludar()  # Imprime: Hola, soy María y tengo 25 años.
+
+""" 
+En este ejercicio, se ha creado la clase Persona con los atributos __nombre y __edad, 
+y un método saludar que imprime un saludo personalizado con el nombre y la edad de la persona. 
+
+Luego, se crearon dos instancias de la clase Persona con diferentes nombres y edades, 
+y se llamó al método saludar para cada instancia para verificar que se imprima correctamente el saludo. """
