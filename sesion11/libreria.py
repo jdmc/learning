80b03a9a9407dcbe70ed33abd1d8f201af4cf61d
jdmc <josedmc@hotmail.com>
2024-02-26 13:48:50 +0000

    Feb 26, 2024, 2:48 PM

--- sesion11/libreria.py
+++ sesion11/libreria.py
@@ -1,12 +1,15 @@
 from libro import Libro
+from libreria import Libreria
 
-class Libreria:
+if __name__ == "__main__":
 
-    def __init__(self, *libros: tuple) -> None:
-        self.__libros = list()
+    quijote = Libro("Don Quijote de la Mancha", "M. Cervantes", "1575")
+    historia_interminable = Libro("Historia Interminable", "M. Ende", "1984")
+
+    libreria = Libreria(quijote, historia_interminable)
+
+    print(len(libreria))
+
+    libreria.anyadir_libro(Libro("El mundo azul", "J. Perez", "1991"))
     
-    def anyadir_libro(self, libro: Libro):
-        self.__libros.append(libro)
-
-    def __len__(self):
-        return len(self.__libros)
+    print(len(libreria))
--- sesion11/libro.py
+++ sesion11/libro.py
@@ -1,6 +1,4 @@
-class Libro:
-
-        
+class Libro:       
         
     #getters + setters 
     
