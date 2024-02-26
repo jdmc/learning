84d0b101ae13579c1c319229cd63fac9e4287253
jdmc <josedmc@hotmail.com>
2024-02-26 13:39:38 +0000

    Feb 26, 2024, 2:39 PM

--- sesion11/ejercicio_basico3.py
+++ sesion11/ejercicio_basico3.py
@@ -14,51 +14,8 @@ Define una clase llamada Libro con los siguientes atributos privados:
     
      """
      
-class Libro:
-    def __init__(self, titulo: str, autor: str, anio: str) -> None:
-        self.__titulo = titulo
-        self.__autor = autor
-        self.__anio = anio
-        
-        
-    #getters + setters 
-    
-    #getter    
-    @property
-    def titulo(self):
-        return self.__titulo
-    
-    @property
-    def autor(self):
-        return self.__autor
-    
-    @property
-    def anio(self):
-        return self.__anio
-    
-    #setter
-    
-    @titulo.setter
-    def titulo(self, valor):
-        self.__titulo = valor
-        
-    @autor.setter
-    def autor(self, valor):
-        self.__autor = valor
-        
-    @anio.setter
-    def anio(self, valor):
-        self.__anio = valor
-        
-        
-        
-        
-    
-    def obtener_titulo(self):
-        return self.__titulo
-    
-    def obtener_autor(self):
-        return self.__autor
-    
-    def obtener_anio(self):
-        return self.__anio
+from libro import Libro
+
+if __name__ == "__main__":
+
+    quijote = Libro()
---
+++ sesion11/libro.py
@@ -0,0 +1,53 @@
+class Libro:
+    def __init__(self, titulo: str, autor: str, anio: str) -> None:
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__anio = anio
+        
+        
+    #getters + setters 
+    
+    #getter    
+    @property
+    def titulo(self):
+        return self.__titulo
+    
+    @property
+    def autor(self):
+        return self.__autor
+    
+    @property
+    def anio(self):
+        return self.__anio
+    
+    #setter
+    
+    @titulo.setter
+    def titulo(self, valor):
+        self.__titulo = valor
+        
+    @autor.setter
+    def autor(self, valor):
+        self.__autor = valor
+        
+    @anio.setter
+    def anio(self, valor):
+        self.__anio = valor
+        
+    def __init__(self, titulo: str, autor: str, anio_publicacion: str ) -> None:
+        self.__titulo = titulo
+        self.__autor = autor
+        self.__anio_publicacion = anio_publicacion
+
+    def detalles_libro(self):
+        return f"{self.titulo} - {self.autor} - {self.anio}"
+        
+    
+    def obtener_titulo(self):
+        return self.__titulo
+    
+    def obtener_autor(self):
+        return self.__autor
+    
+    def obtener_anio(self):
+        return self.__anio
