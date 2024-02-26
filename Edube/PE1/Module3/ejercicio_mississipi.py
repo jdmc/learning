2ba203f9f60220c5f34943fa274ba3337d1c6efb
jdmc <josedmc@hotmail.com>
2024-02-26 08:19:44 +0000

    Feb 26, 2024, 9:19 AM

--- Edube/PE1/Module3/ejercicio_mississipi.py
+++ Edube/PE1/Module3/ejercicio_mississipi.py
@@ -11,3 +11,21 @@ for i in range(1, 6):
     print( i , "Mississippi", end=" ")
     
 print("¡Listos o no, ahí voy!")
+
+
+
+# Ejercicio while / break
+
+while True:
+    palabra = input("ingerese palabras")
+    if palabra == "chupacabra":
+        print("¡Has dejado el bucle con éxito.")
+    break
+
+# Ejercicio Continue
+
+# Indicar al usuario que ingrese una palabra
+# y asignarlo a la variable user_word.
+
+for letter in user_word:
+    # Completa el cuerpo del bucle for.
