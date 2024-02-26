aaa8907fb53b73978c5f38422d62a8ff66cb3496
jdmc <josedmc@hotmail.com>
2024-02-26 12:30:03 +0000

    Feb 26, 2024, 1:30 PM

--- sesion11/ejercicio_basico1.py
+++ sesion11/ejercicio_basico1.py
@@ -36,3 +36,45 @@ y se llamó al método saludar para cada instancia para verificar que se imprima
 
 # Version Ricardo
 
+class Persona:
+
+    #getters
+    @property
+    def nombre(self) -> str:
+        return self.__nombre
+    
+    @property
+    def edad(self) -> str:
+        return self.__edad
+
+    def __init__(self, nombre: str, edad: int) -> None:
+        self.__nombre = nombre
+        self.__edad = edad
+
+    def saludar(self):
+        return f"Hola {self.__nombre}"
+    
+
+
+juan = Persona('Juan', 21)
+belen = Persona('Belen', 23)
+
+print(juan.saludar())
+print(belen.saludar())
+
+"""
+La elección entre usar print() o return en un método como saludar() de una clase en Python depende del propósito y el contexto en el que estés utilizando el método.
+
+Uso de print(): print() es útil cuando quieres que el método muestre un mensaje directamente en la consola o en algún otro flujo de salida. 
+Esto es útil cuando estás interactuando con el programa a través de la línea de comandos o necesitas que la salida sea visible para el usuario.
+
+Uso de return: return es útil cuando quieres que el método devuelva un valor para que pueda ser utilizado en otro lugar del programa. 
+Esto es útil cuando necesitas que el resultado del método se pueda usar para realizar alguna operación posterior o para mostrarlo en otra parte de la interfaz de usuario (por ejemplo, en una interfaz gráfica).
+
+En el caso del método saludar(), si solo estás interesado en mostrar un saludo en la consola o en algún flujo de salida, usar print() es apropiado. 
+Por otro lado, si deseas utilizar el saludo en otro lugar del programa o necesitas manipularlo de alguna manera, entonces usar return sería más adecuado.
+
+Aquí hay un ejemplo que ilustra cómo se usaría print() y return en el método saludar():
+
+
+"""
