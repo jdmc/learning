81e4714a09fdc5569723ec8e537dd4b2295f9cb7
jdmc <josedmc@hotmail.com>
2024-02-27 12:20:08 +0000

    Feb 27, 2024, 1:20 PM

--- sesion3/listas1.py
+++ sesion3/listas1.py
@@ -1,28 +1,4 @@
-""" Listas en Python
 
-Las listas en Python son un tipo de dato que se utiliza para almacenar una colección ordenada y mutable de elementos.
-
-Las listas se definen entre corchetes y los elementos se separan por comas.
-
-mi_lista = ["Hola", 1, True]
-
-En este ejemplo, se crea una lista con tres elementos: una cadena, un número entero y un valor booleano.
-
-Las listas son muy versátiles y se pueden usar para almacenar cualquier tipo de dato, incluso otras listas.
-
-Las listas son mutables, lo que significa que se pueden modificar los elementos de una lista después de que se ha creado.
-
-Se puede agregar, eliminar o modificar elementos de una lista.
-
-Las listas son una herramienta poderosa para organizar y almacenar datos en Python.
-
-Aquí hay algunos ejemplos de cómo se pueden usar las listas:
-
-Almacenar una lista de nombres.
-Almacenar una lista de números.
-Almacenar una lista de tareas.
-
-Las listas son una herramienta fundamental para el desarrollo de aplicaciones en Python. """
 
 
 # valores_booleanos = [] #lista vacia
@@ -44,42 +20,6 @@ for valor_booleano in valores_booleanos:
     print(numero_trues, numero_falses)  
 
 print(f"Numero de trues:{numero_trues}", f"Numero de falses:{numero_falses}", sep=" --- ")
-
-
-"""
-** Append en Python
-
-append() es un método que se utiliza para agregar un elemento al final de una lista en Python.
-
-El método append() toma un solo argumento, que es el elemento que se desea agregar a la lista.
-
-Ejemplo:
-
-*****************************
-
-mi_lista = ["Hola", 1, True]
-
-mi_lista.append("nuevo elemento")
-
-print(mi_lista)
-
-**************************
- En este ejemplo, se agrega el elemento "nuevo elemento" al final de la lista mi_lista. 
-
-La lista final será ["Hola", 1, True, "nuevo elemento"].
-
-El método append() es una forma sencilla de agregar elementos a una lista.
-
-Es importante tener en cuenta que el método append() no devuelve ningún valor.
-
-Aquí hay algunos ejemplos adicionales de cómo se puede usar el método append():
-
-Agregar un número al final de una lista de números.
-Agregar una cadena al final de una lista de cadenas.
-Agregar una lista al final de una lista de listas.
-
-El método append() es una herramienta útil para trabajar con listas en Python. """
-
 
 #programacion = ["python", "Java", "Kotlin", "Typescript", "C#"]
 
@@ -110,9 +50,6 @@ def eliminar_duplicados(lista_lenguajes):
         """
     
     return lista_sin_duplicados
-
-
-
 
 """
 Pedir un lenguaje al usuario
@@ -158,8 +95,6 @@ if programacion_normalizada_sin_duplicados != None:
             programacion_normalizada_sin_duplicados.append(lenguaje.lower()) if lenguaje.lower() not in programacion_normalizada_sin_duplicados else None # None equivale a nop (no operation) 
             #lenguaje not in programacion and programacion.append(lenguaje) 
             
-
-
 #despues
 listar_lenguajes(programacion_normalizada_sin_duplicados)        
    
