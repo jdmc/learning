6528062ef235ad24c50eab7b9bd1d2b6c879404a
jdmc <josedmc@hotmail.com>
2024-02-28 10:49:56 +0000

    Feb 28, 2024, 11:49 AM

---
+++ sesion13/ejercicio_herencia.py
@@ -0,0 +1,13 @@
+""" Herencia en Python 
+1.Define una clase base llamada Vehiculo con los siguientes atributos: 
+    •marca: la marca del vehículo. 
+    •modelo: el modelo del vehículo. 
+    •anno: el año de fabricación del vehículo. 
+2.Implementa un método en la clase Vehiculo llamado mostrar_informacion que imprima la marca, el modelo y el año del vehículo. 
+3.Crea una clase derivada llamada Automovil que herede de la clase Vehiculo. 
+    Agrega un atributo adicional: 
+    •tipo_carroceria: el tipo de carrocería del automóvil (por ejemplo, "sedán", "SUV", etc.). 
+4.Implementa un método en la clase Automovil llamado arrancar que imprima un mensaje indicando que el automóvil está arrancando. 
+5.Crea una instancia de la clase Automovil con información específica de un automóvil, como marca, modelo, año y tipo de carrocería. 
+6.Utiliza el método mostrar_informacion de la clase base para mostrar los detalles del automóvil. 
+7.Llama al método arrancar para simular el arranque del automóvil. """
