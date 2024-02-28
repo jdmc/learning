fcf567049fa115815bfea6c5c2ca8bd7c81fa4e2
jdmc <josedmc@hotmail.com>
2024-02-28 12:01:05 +0000

    Feb 28, 2024, 1:01 PM

--- sesion13/Garaje/automovil.py
+++ sesion13/Garaje/automovil.py
@@ -1,4 +1,11 @@
 from vehiculo import Vehiculo
+from enum import Enum
+
+class TIPO_CARROCERIA(Enum):
+    SEDAN= 1,
+    BERLINER = 2,
+    SUV = 3,
+    TT = 4
 
 class Automovil (Vehiculo):
     def __init__(self,  marca: str, modelo: str, anno: str, tipo_carroceria: str):
