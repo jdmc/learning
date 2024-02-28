46911e1775d01a25ecdbd462b08940cce98b5642
jdmc <josedmc@hotmail.com>
2024-02-28 10:53:18 +0000

    Feb 28, 2024, 11:53 AM

--- sesion13/granja/gallina.py
+++ sesion13/granja/gallina.py
@@ -3,4 +3,4 @@ from animal import Animal
 class Gallina(Animal):
     
     def comer(self):
-        super().comer() + "y picotear como una gallina"
+        return super().comer() + "y picotear como una gallina"
