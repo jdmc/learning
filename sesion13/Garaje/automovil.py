6cc4f9b06de16274fdec93312f226318dd5b56bd
jdmc <josedmc@hotmail.com>
2024-02-28 12:05:43 +0000

    Feb 28, 2024, 1:05 PM

--- sesion13/Garaje/automovil.py
+++ sesion13/Garaje/automovil.py
@@ -12,6 +12,9 @@ class Automovil (Vehiculo):
         super().__init__(marca, modelo, anno)
         self.__tipo_carroceria = tipo_carroceria
         
+    def mostrar_informacion (self,):        
+        return f"{super().mostrar_informacion()} {self.__tipo_carroceria}"
+        
         
     def arrancar(self):
         return f"El coche de la marca {self._marca} esta arrancando....:"
