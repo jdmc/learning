f0ffdc6eb8e275d093c099142db46e8bd2e41103
jdmc <josedmc@hotmail.com>
2024-02-28 13:12:01 +0000

    Feb 28, 2024, 2:12 PM

--- sesion12/gestion_bancaria/cliente.py
+++ sesion12/gestion_bancaria/cliente.py
@@ -1,10 +1,9 @@
 class Cliente:
-    
     def __init__(self, nombre:str, dni:str) -> None:
-        self.__nombre = nombre
-        self.__dni = dni
-        pass
-    
-    #magic
-        def __str__(self):
-            return f"{self.__nombre}|{self.__dni}"
+        # Inicializar un nuevo cliente con un nombre y un DNI
+        self.__nombre = nombre  # Atributo privado para el nombre del cliente
+        self.__dni = dni  # Atributo privado para el DNI del cliente
+
+    # MAGIC >> Método mágico para devolver una representación legible del cliente
+    def __str__(self):
+        return f"{self.__nombre}|{self.__dni}"
