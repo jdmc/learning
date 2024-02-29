0ba0f9c6d31783f9b35fe900d4f47168cb1de34c
jdmc <josedmc@hotmail.com>
2024-02-29 11:59:13 +0000

    Feb 29, 2024, 12:59 PM

---
+++ sesion14/herencias/ejercicio14.py
@@ -0,0 +1,13 @@
+
+# Herencia con Listas en Python 
+
+# 1.Define una clase base llamada ListaNumeros que tenga como atributo una lista de números. 
+# 2.Implementa un método en la clase ListaNumeros llamado sumar_elementos que calcule la suma de todos los números en la lista. 
+# 3.Crea una clase derivada llamada ListaOperaciones que herede de ListaNumeros. Añade un atributo adicional: 
+#   • operacion: una cadena que represente la operación a realizar en la lista (por ejemplo, "multiplicar", "restar", etc.). 
+# 4.Implementa un método en la clase ListaOperaciones llamado realizar_operacion que, según el valor de operacion, realice la operación correspondiente en la lista de números y devuelva el resultado. 
+# 5.Crea una instancia de la clase ListaOperaciones con una lista de números y una operación específica. 
+# 6.Utiliza el método sumar_elementos de la clase base para mostrar la suma original de los elementos. 
+# 7.Llama al método realizar_operacion para realizar la operación seleccionada y muestra el resultado.
+
+
