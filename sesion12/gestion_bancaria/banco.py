88a6a4360f79e82535f32375983c4a18f14b05c8
jdmc <josedmc@hotmail.com>
2024-02-29 10:16:02 +0000

    Feb 29, 2024, 11:16 AM

--- sesion12/gestion_bancaria/banco.py
+++ sesion12/gestion_bancaria/banco.py
@@ -13,8 +13,24 @@ class Banco:
 
     def agregar_cuenta(self, cuenta: Cuenta):
         # Método para agregar una cuenta al banco
-        if cuenta.numero_cuenta in [cuenta.numero_cuenta for cuenta in self.cuentas]:
+        if numero_cuenta in [cuenta.numero_cuenta for cuenta in self.cuentas]:
             # Verificar si el número de cuenta ya existe en el banco
             raise CuentaExistenteError(cuenta.numero_cuenta)  # Lanzar una excepción si el número de cuenta ya existe
         self.__cuentas.append(cuenta)  # Agregar la cuenta a la lista de cuentas del banco
+        
 
+        
+""" 
+Me daba error en la parte principal:
+
+File "c:\Users\josed\Documents\Formación\PUE\Python\Learning\sesion12\gestion_bancaria\banco.py", line 16, in agregar_cuenta
+    if cuenta.numero_cuenta in [cuenta.numero_cuenta for cuenta in self.cuentas]:
+       ^^^^^^^^^^^^^^^^^^^^
+AttributeError: 'Cuenta' object has no attribute 'numero_cuenta'
+    
+    En la línea 16 if cuenta.numero_cuenta in [cuenta.numero_cuenta for cuenta in self.cuentas]:, 
+    el error se produce porque intento acceder al atributo numero_cuenta de cada objeto cuenta en la lista self.cuentas. 
+    Sin embargo, debería acceder al atributo numero_cuenta de cada objeto Cuenta en la lista self.cuentas. 
+    Esto se puede corregir simplemente eliminando el cuenta. del primer cuenta.numero_cuenta.
+
+ """
