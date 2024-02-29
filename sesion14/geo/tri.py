10bbeec195763f222affef175b17abf2bfc464fb
jdmc <josedmc@hotmail.com>
2024-02-29 11:38:21 +0000

    Feb 29, 2024, 12:38 PM

--- sesion14/geo/tri.py
+++ sesion14/geo/tri.py
@@ -3,7 +3,7 @@ from figura import FiguraGeometrica
 class Triangulo (FiguraGeometrica):
         
     def __init__ (self, lado1, lado2, lado3, altura):
-        super().__init__ (lado1,lado2)
+        super().__init__ (lado1, lado2)
         self.altura = altura
         self.lado3 = lado3
         
