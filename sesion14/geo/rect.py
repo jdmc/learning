c95fb0c4bd82dce0aeb0cf8cfdd48a2f96a46794
jdmc <josedmc@hotmail.com>
2024-02-29 11:08:40 +0000

    Feb 29, 2024, 12:08 PM

--- sesion14/geo/rect.py
+++ sesion14/geo/rect.py
@@ -2,9 +2,8 @@ from ejercicio_geomet import FiguraGeometrica
 
 class Rectangulo(FiguraGeometrica):
         
-    def __init__(self, lado1, lado2, lado3):
-        super().__init__ (lado1,lado2)
-        self.__lado3 = lado3
+    def __init__(self, lado1, lado2):
+        super().__init__ (lado1, lado2)
         
     def calcular_area(self):
-        return super().calcular_area()
+        area= self.lado1 * self.lado2
