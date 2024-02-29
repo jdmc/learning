6f4e449cea95de16db16aa309c0c2dea4f191ed6
jdmc <josedmc@hotmail.com>
2024-02-29 10:58:20 +0000

    Feb 29, 2024, 11:58 AM

--- sesion14/ejercicio_geomet.py
+++ sesion14/ejercicio_geomet.py
@@ -51,9 +51,15 @@ class Rectangulo(FiguraGeometrica):
     def __init__(self, lado1, lado2, lado3):
         super().__init__ (lado1,lado2)
         self.__lado3 = lado3
+        
+    def calcular_area(self):
+        return super().calcular_area()
             
 class Triangulo (FiguraGeometrica):
         
     def __init__ (self, lado1, lado2, altura):
         super().__init__ (lado1,lado2)
         self.__altura = altura
+        
+    def calcular_area(self):
+        return super().calcular_area()
