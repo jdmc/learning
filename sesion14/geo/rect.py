5281f3dafa39f272fa53c95fc5471fcb47c06446
jdmc <josedmc@hotmail.com>
2024-02-29 11:20:49 +0000

    Feb 29, 2024, 12:20 PM

--- sesion14/geo/rect.py
+++ sesion14/geo/rect.py
@@ -6,5 +6,6 @@ class Rectangulo(FiguraGeometrica):
         super().__init__ (lado1, lado2)
         
     def calcular_area(self):
+        super.calcular_area()
         area= self.lado1 * self.lado2
         return area
--- sesion14/geo/tri.py
+++ sesion14/geo/tri.py
@@ -9,5 +9,6 @@ class Triangulo (FiguraGeometrica):
         
 
     def calcular_area(self):
+        super.calcular_area()
         area= self.lado2 * self.altura * 0.5
         return area
