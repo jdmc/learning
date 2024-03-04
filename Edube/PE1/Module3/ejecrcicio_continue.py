a23a00cc6e2b5673a17a41911d4db0931e4109f3
jdmc <josedmc@hotmail.com>
2024-03-04 10:54:54 +0000

    Mar 4, 2024, 11:54 AM

--- Edube/PE1/Module3/ejecrcicio_continue.py
+++ Edube/PE1/Module3/ejecrcicio_continue.py
@@ -11,6 +11,7 @@ Utiliza user_word = user_word.upper() para convertir la palabra ingresada por el
 Utiliza la ejecución condicional y la instrucción continue para "comer" las siguientes vocales A , E , I , O , U de la palabra ingresada.
 Imprime las letras no consumidas en la pantalla, cada una de ellas en una línea separada. """
 
+# crear una lista de las vocales
 eat = ['A', 'E', 'I', 'O', 'U']
 
 # solicitará al usuario que ingrese una palabra
@@ -21,6 +22,7 @@ user_word = user_word.upper()
 
 # recorrerá cada letra de la palabra
 for letter in user_word:
+    #Si la letra es una vocal (A, E, I, O, U), se ignorará y el bucle continuará con la siguiente letra.
     if letter in eat:
         continue
     print (letter)
