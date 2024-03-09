639b1d30c6338517b43b4e8aa9371f8ebb0e522a
jdmc <josedmc@hotmail.com>
2024-03-09 15:59:21 +0000

    Mar 9, 2024, 4:59 PM

--- exam prep/compreshensionlist.py
+++ exam prep/compreshensionlist.py
@@ -6,3 +6,17 @@ numeros_pares = [num for num in numeros if num % 2 == 0]
 
 # Imprimiendo la lista de números pares
 print("Números pares:", numeros_pares)
+
+
+# Lista original
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Utiliza la comprensión de listas para filtrar los números impares
+
+numeros_impares = [impares for impares in numeros if impares % 2 != 0]
+
+print ("*" * 50)
+
+print("Números impares:", numeros_impares)
+
+
