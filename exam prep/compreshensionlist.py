a6e7814acc21318408e2f8e3872496c99c693323
jdmc <josedmc@hotmail.com>
2024-03-09 15:54:50 +0000

    Mar 9, 2024, 4:54 PM

---
+++ exam prep/compreshensionlist.py
@@ -0,0 +1,8 @@
+# Lista original
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Utilizando comprensión de listas para filtrar los números pares
+numeros_pares = [num for num in numeros if num % 2 == 0]
+
+# Imprimiendo la lista de números pares
+print("Números pares:", numeros_pares)
