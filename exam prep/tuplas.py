8a5f9e7cc50fcb73aa4d1d4bf831ec2d6dc39d91
jdmc <josedmc@hotmail.com>
2024-03-10 15:05:57 +0000

    Mar 10, 2024, 4:05 PM

--- exam prep/tuplas.py
+++ exam prep/tuplas.py
@@ -6,3 +6,10 @@ estudiantes = [("Juan", 75), ("María", 50), ("Carlos", 80), ("Ana", 65), ("Pedr
 cracks = [ aprobado[0] for aprobado in estudiantes if aprobado[1] >= 60 ]
 
 print(" Estudiantes cracks con 60 o más puntiación:", cracks)
+
+# Lista de tuplas (nombre del producto, precio, cantidad en stock)
+productos = [("Camisa", 25, 10), ("Pantalón", 60, 5), ("Zapatos", 80, 0), ("Bufanda", 40, 3), ("Gorra", 20, 8)]
+
+# Utiliza la comprensión de listas para filtrar los nombres de los productos que cumplen con las condiciones
+
+caro = []
