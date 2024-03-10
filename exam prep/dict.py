a9a39a5f98a35ad692e4e9c8bfcf29d5924ad08f
jdmc <josedmc@hotmail.com>
2024-03-10 16:53:33 +0000

    Mar 10, 2024, 5:53 PM

--- exam prep/dict.py
+++ exam prep/dict.py
@@ -5,3 +5,29 @@ personas = [("Juan", 30), ("María", 25), ("Carlos", 35), ("Ana", 28)]
 mi_dict = dict(personas)
 
 print(mi_dict)
+
+print("@" *50)
+
+# Lista de nombres
+nombres = ["Juan", "María", "Carlos", "Ana", "Juan", "María", "Juan", "Carlos", "María"]
+
+# Crea un diccionario donde las claves sean los nombres y los valores sean la cantidad de veces que aparece cada nombre
+
+contar_nombres= {}
+
+for clave in nombres: 
+    if clave in contar_nombres:
+        contar_nombres[clave] += 1
+    else: 
+        contar_nombres[clave] = 1
+
+print (contar_nombres)
+
+# Lista de nombres de frutas
+frutas = ["Manzana", "Banana", "Naranja", "Pera"]
+
+# Lista de precios correspondientes
+precios = [2.50, 1.20, 1.80, 2.00]
+
+# Crea un diccionario donde las claves sean los nombres de las frutas y los valores sean los precios de cada fruta
+
