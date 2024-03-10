86899eb9224e2ce89f6aaf404f75b331f9acbac9
jdmc <josedmc@hotmail.com>
2024-03-10 14:53:20 +0000

    Mar 10, 2024, 3:53 PM

--- exam prep/tuplas.py
+++ exam prep/tuplas.py
@@ -2,3 +2,7 @@
 estudiantes = [("Juan", 75), ("María", 50), ("Carlos", 80), ("Ana", 65), ("Pedro", 55)]
 
 # Utiliza la comprensión de listas para filtrar los nombres de los estudiantes que aprobaron el examen
+
+cracks = [ aprobado for aprobado in estudiantes if estudiantes (int( )) >= 60 ]
+
+print(" Estudiantes cracks con 60 o más puntiación:", cracks)
