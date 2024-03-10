55568b4d122dd242dd82d6d128f4725f61fb66a2
jdmc <josedmc@hotmail.com>
2024-03-10 16:31:46 +0000

    Mar 10, 2024, 5:31 PM

---
+++ exam prep/dict.py
@@ -0,0 +1,4 @@
+# Lista de tuplas (nombre, edad)
+personas = [("Juan", 30), ("María", 25), ("Carlos", 35), ("Ana", 28)]
+
+# Crea un diccionario donde las claves sean los nombres y los valores sean las edades
