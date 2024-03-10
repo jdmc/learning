8030559d02acc8fd35bc77af1e263ce7b7873aeb
jdmc <josedmc@hotmail.com>
2024-03-10 15:24:04 +0000

    Mar 10, 2024, 4:24 PM

--- exam prep/tuplas.py
+++ exam prep/tuplas.py
@@ -15,3 +15,16 @@ productos = [("Camisa", 25, 10), ("Pantalón", 60, 5), ("Zapatos", 80, 0), ("Buf
 caro = [quiero[0] for quiero in productos if quiero[1] >50 and quiero[2] > 0 ]
 
 print ("Quiero producto caro y disponible:", caro )
+
+
+# Lista de tuplas (nombre del estudiante, lista de calificaciones)
+estudiantes = [("Juan", [85, 92, 88]), ("María", [75, 80, 95]), ("Carlos", [90, 85, 88]), ("Ana", [85, 90, 95]), ("Pedro", [80, 85, 90])]
+
+# Utiliza la comprensión de listas para filtrar los nombres de los estudiantes que cumplen con las condiciones 1 >= 90
+
+crack = [coco[0] for coco in estudiantes if coco[1] (nota >= 90 for nota in coco[1])]
+
+print ("/"*50)
+
+print( "Coco bello más de 90:", crack)
+
