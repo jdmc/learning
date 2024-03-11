d36d12847e0d962009aaef9f7301b686796a1a2b
jdmc <josedmc@hotmail.com>
2024-03-11 07:41:34 +0000

    Mar 11, 2024, 8:41 AM

---
+++ exam prep/def.py
@@ -0,0 +1,10 @@
+""" Escribe una función llamada saludar que tome un nombre como argumento y devuelva un saludo con ese nombre. 
+Por ejemplo, si se pasa el nombre "Juan" a la función, debería devolver "¡Hola, Juan!". """
+
+def saludar(nombre):
+    saludo = "¡Hola " + nombre + "!" 
+    return saludo
+
+nombre = Juan
+print(saludar(nombre))
+    
