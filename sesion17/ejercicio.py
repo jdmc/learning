c4956ec7eea9aa724fbbc1c87b045a1a306af39c
jdmc <josedmc@hotmail.com>
2024-03-12 12:25:35 +0000

    Mar 12, 2024, 1:25 PM

---
+++ sesion17/ejercicio.py
@@ -0,0 +1,7 @@
+def ejecutar(self):
+        """Presentar menu de usuario que permita las opciones:
+        1.- Agregar contacto
+        2.- Mostrar contactos
+        3.- Salir
+        """
+        pass
