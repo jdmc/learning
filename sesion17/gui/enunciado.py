6490ea9c92b6339fae3e29ece9845db1378ed3ce
jdmc <josedmc@hotmail.com>
2024-03-12 13:32:04 +0000

    Mar 12, 2024, 2:32 PM

--- sesion17/gui/enunciado.py
+++ sesion17/gui/enunciado.py
@@ -3,3 +3,7 @@ Desarrolla una aplicación de gestión de tareas con interfaz gráfica utilizand
 La aplicación debe permitir al usuario agregar nuevas tareas con una descripción, fecha de vencimiento y prioridad. 
 Además, debería ser posible marcar las tareas como completadas y eliminarlas. 
 Asegúrate de proporcionar una interfaz intuitiva y agradable para una experiencia de usuario óptima. """
+
+import tkinter as tk
+from tkinter import ttk
+
