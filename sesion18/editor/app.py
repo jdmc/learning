56bd5b9eef94b78a1b24aea098b53454db56de11
jdmc <josedmc@hotmail.com>
2024-03-13 12:52:02 +0000

    Mar 13, 2024, 1:52 PM

--- sesion18/editor/app.py
+++ sesion18/editor/app.py
@@ -1,6 +1,6 @@
 import os
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 
 text_contents = dict()
 
@@ -14,11 +14,12 @@ def check_for_changes():
             notebook.tab("current", text=name + "*")
     elif name[-1] == "*":
         notebook.tab("current", text=name[:-1])
-        
+
 def get_text_widget():
     text_widget = notebook.nametowidget(notebook.select())
 
-    return text_widget       
+    return text_widget
+
 
 def create_file(content="", title="Sin titulo"):
     text_area = tk.Text(notebook)
@@ -40,32 +41,55 @@ def open_file():
         return
     
     create_file(content=text, title=file_name)
-    
+
 def save_file():
     file_path = filedialog.asksaveasfilename()
     try:
         file_name = os.path.basename(file_path)
-        text_widget = get_text_widget()        
+        text_widget = get_text_widget()
         text = text_widget.get("1.0", "end-1c")
         
         with open(file_path, "w") as file:
             file.write(text)
-            
+
     except (AttributeError, FileNotFoundError):
         print("Guardado cancelado")
         return
     
     notebook.tab("current", text=file_name)
     text_contents[str(text_widget)] = hash(text)
+
+
+def confirm_quit():
     
-def close_file():
-    file_path = filedialog.asksaveasfilename()
-    pass
+    unsaved = False
+
+    for tab in notebook.tabs():
+        text_widget = root.nametowidget(tab)
+        content = text_widget.get("1.0", "end-1c")
+
+        if hash(content) != text_contents[str(text_widget)]:
+            unsaved = True
+            break
+    
+    if unsaved:
+        confirm = messagebox.askyesno(
+            message="¿Desea salir sin guardar?",
+            icon="question",
+            title="Salir sin guardar"
+        )
+        if not confirm:
+            return
+    
+    root.destroy()
+
+def close_current_tab():
+    notebook.forget(notebook.select())
 
 root = tk.Tk()
 root.geometry("300x300")
 root.title("Editor de texto")
-root.option_add("tearOff", False)
+root.option_add("*tearOff", False)
 
 main = ttk.Frame(root)
 main.pack(side="left", fill="both", expand=True, padx=(1), pady=(4,0))
@@ -81,8 +105,8 @@ menu_bar.add_cascade(label="Archivo", menu=file_menu)
 file_menu.add_command(label="Nuevo", command=create_file, accelerator="Ctrl+N")
 file_menu.add_command(label="Abrir", command=open_file, accelerator="Ctrl+O")
 file_menu.add_command(label="Guardar", command=save_file, accelerator="Ctrl+S")
-file_menu.add_command(label="Cerrar", command=close_file, accelerator="Ctrl+Q")
-
+file_menu.add_command(label="Cerrar tab", command=close_current_tab, accelerator="Ctrl+Q")
+file_menu.add_command(label="Salir", command=confirm_quit)
 
 #creacion del notebook
 notebook = ttk.Notebook(main)
@@ -91,8 +115,15 @@ notebook.pack(fill="both", expand=True)
 create_file()
 
 root.bind("<KeyPress>", lambda event: check_for_changes())
-root.bind("<Control-n>", lambda e: create_file())
-root.bind("<Control-o>", lambda e: open_file())
-root.bind("<Control-s>", lambda e: save_file())
+root.bind("<Control-n>", lambda event: create_file())
+root.bind("<Control-o>", lambda event: open_file())
+root.bind("<Control-q>", lambda event: close_current_tab())
+root.bind("<Control-s>", lambda event: save_file())
 
 root.mainloop()
+
+
+"""
+Añadir la opcion de ABRIR FICHERO asociada a una funcion llamada open_file que cargue
+el contenido.
+"""
