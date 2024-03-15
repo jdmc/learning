b5333efd216699f8badccacb62cb18e14cf259b5
jdmc <josedmc@hotmail.com>
2024-03-15 12:16:36 +0000

    Mar 15, 2024, 1:16 PM

--- sesion20/nearby.py
+++ sesion20/nearby.py
@@ -10,3 +10,13 @@ for clave, dict_anidado in datos.items():
     print(clave)
     for clave2, valor2 in dict_anidado.items():
         print(f"{clave2!r}: {valor2!r}")
+        
+
+""" La letra r dentro de una cadena de formato en Python indica que el valor debería ser representado utilizando su representación "repr" en lugar de su representación "str". 
+La representación "repr" intenta mostrar el valor de una manera que sea lo más cercana posible a su definición en el código fuente, 
+mientras que la representación "str" está más orientada hacia la legibilidad humana. 
+Esto es útil para depurar y entender exactamente qué contiene una variable, especialmente si es un objeto complejo.
+
+
+El !r dentro de la cadena de formato indica que el valor debe ser representado utilizando su representación "repr". 
+En otras palabras, !r le dice a Python que utilice la función repr() para obtener una representación del objeto en lugar de la función str(). """
