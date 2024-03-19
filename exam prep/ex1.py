3b1574242e269891e6fdde405d40f7da12946416
jdmc <josedmc@hotmail.com>
2024-03-19 10:47:28 +0000

    Mar 19, 2024, 11:47 AM

--- exam prep/ex1.py
+++ exam prep/ex1.py
@@ -1,16 +1,23 @@
-def cadena (texto):
+def separar (texto):
     palabras = texto.split()
     return palabras
     
 
-def lista (texto):
-    pass
+def cadena (lista):
+    lista_palabra = lista.join(",")
+    return lista_palabra
 
 def combo (texto):
     pass
     
 
-ejemplo ="Esto es una cadena de texto"
+ejemplo1 ="Esto es una cadena de texto"
 
-palabras_split = cadena(ejemplo)
+palabras_split = separar(ejemplo1)
 print(palabras_split)    
+
+
+ejemplo2 =["Esto, es, una, lista, de, palabras"]
+
+palabras_join = cadena(ejemplo2)
+print(palabras_join)
