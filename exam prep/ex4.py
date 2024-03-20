6759c0583b73faee26035c9c519b2b95ef65f526
jdmc <josedmc@hotmail.com>
2024-03-20 09:12:27 +0000

    Mar 20, 2024, 10:12 AM

--- exam prep/ex4.py
+++ exam prep/ex4.py
@@ -1,5 +1,5 @@
-class Vehiculo:
-    def __init__(self, marca, modelo, anno, color):
+class Vehiculo: # clase que representa un vehiculo
+    def __init__(self, marca, modelo, anno, color): # constructor de la clase vehiculo
         self._marca = marca
         self._modelo = modelo
         self._anno =  anno
