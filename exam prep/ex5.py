b6a3df8bc75cd064aa1c28001e0b915eaee461ce
jdmc <josedmc@hotmail.com>
2024-03-20 09:27:49 +0000

    Mar 20, 2024, 10:27 AM

--- exam prep/ex5.py
+++ exam prep/ex5.py
@@ -1,8 +1,10 @@
 class Vehiculo():
-    self._marca = marca
-    self._modelo = modelo
-    self._anno =  anno
-    self._color = color
+    def __init__(self, marca, modelo, anno, color): # constructor de la clase vehiculo
+        #parametros de la clase
+        self._marca = marca
+        self._modelo = modelo
+        self._color = color
+        self._anno =  anno
 
 class Coches(Vehiculo):
     pass
