cf969cb34d184131d8783e74e41d6095b432fe98
jdmc <josedmc@hotmail.com>
2024-04-24 11:52:04 +0000

    Apr 24, 2024, 1:52 PM

--- exam prep/test.py
+++ exam prep/test.py
@@ -1,4 +1,4 @@
-class A:
+""" class A:
     def a(self):
         print("A", end="")
         def b(self):
@@ -15,7 +15,18 @@ class C(A):
             self.b()
                         
 B().do()
-C().do()
+C().do() """
+
+
+def fun(n):
+    s = '+'
+    for i in range(n):
+        s += s
+        yield s
+
+
+for x in fun(2):
+    print(x, end='')
 
 
 
