6a68fa550d23eeb06ee1b30db6b78cc91c7a00f9
jdmc <josedmc@hotmail.com>
2024-04-25 18:19:45 +0000

    Apr 25, 2024, 8:19 PM

--- exam prep/test.py
+++ exam prep/test.py
@@ -37,8 +37,20 @@ for x in fun(2):
 print(f(2))
  """
  
-def f(n):
+""" def f(n):
     for i in range(1, n+1):
         yield i
 for i in f(2):
-    print(i, end=" ")
+    print(i, end=" ") """
+    
+s ="2a"
+try:
+    n= int(s)
+except TypeError:
+    n=3
+except LookupError:
+    n=2
+except:
+    n=1
+    print(n)
+        
