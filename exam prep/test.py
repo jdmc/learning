fa81e00240899ce440a72abe72cb677147648112
jdmc <josedmc@hotmail.com>
2024-05-03 10:45:35 +0000

    May 3, 2024, 12:45 PM

--- exam prep/test.py
+++ exam prep/test.py
@@ -184,3 +184,8 @@ print(Object.get())
 my_list = [i for i in range(5)]
 m = [my_list[i] for i in range(4,0,-1) if my_list[i] %2 !=0]
 print(m) """
+
+#133
+def foo (x,y):
+    return y(x) + y(x+1)
+print (foo(1,lambda x:x*x))
